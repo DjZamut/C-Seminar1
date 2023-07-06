952186cd4551f05a2274e2244edc9024e38350da
Timur <agaev24@gmail.com>
2023-07-06 23:54:22 +0300

    Задача 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,37 @@
 // 3 -> Среда
 // 5 -> Пятница
 
+Console.WriteLine("Введите номер от 1 до 7: ");
+string number = Console.ReadLine();
+if (number == "1")
+{
+Console.Write("Понедельник");
+}
+if (number == "2")
+{
+Console.Write("Вторник");
+}
+if (number == "3")
+{
+Console.Write("Среда");
+}
+if (number == "4")
+{
+Console.Write("Четверг");
+}
+if (number == "5")
+{
+Console.Write("Пятница");
+}
+if (number == "6")
+{
+Console.Write("Суббота");
+}
+if (number == "7")
+{
+Console.Write("Воскресенье");
+}
+else
+{
+    System.Console.WriteLine("Error");
+}
