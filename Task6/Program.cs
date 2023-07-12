99d46ac0cc22c7942bfefc89102b4fdf70aac41a
Timur <agaev24@gmail.com>
2023-07-12 16:28:51 +0300

    Задача 6 решена

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int Promt(string message)
+{
+    System.Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void GetResult(int X, int Y)
+{
+    if(X > 0 && Y > 0)
+    {
+        System.Console.WriteLine("I четверть");
+    }
+    if(X < 0 && Y > 0)
+    {
+        System.Console.WriteLine("II четверть");
+    }
+    if(X < 0 && Y < 0)
+    {
+        System.Console.WriteLine("III четверть");
+    }
+    if(X > 0 && Y < 0)
+    {
+        System.Console.WriteLine("IV четверть");
+    }
+}
+
+int x = Promt("Введите точку X: ");
+int y = Promt("Введите точку Y: ");
+GetResult(x, y);
