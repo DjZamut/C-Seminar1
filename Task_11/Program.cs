3ff1c85f6893f8d39f57fbc6ec0185aa8d59b552
Timur <agaev24@gmail.com>
2023-07-16 12:24:52 +0300

    Добавил второй способ решения задачи

--- Task_11/Program.cs
+++ Task_11/Program.cs
@@ -23,3 +23,25 @@ class Program
   return 0;
  }
 }
+
+// Второй способ решения задачи в классе:
+
+
+// int Promt(string message) // создаем метод для печати
+// {
+// Console.Write(message);
+// int num = int.Parse(Console.ReadLine()!);
+// return num;
+// }
+
+// int Razryd(int a)
+// {
+// int count = 0;
+// for (; a > 0; count++) // инициализация переменной
+// {
+// a = a / 10;
+// }
+// return count;
+// }
+// int num = Promt("Введите число: ");
+// Console.WriteLine($"Количество цифр в числе = {Razryd(num)}");
