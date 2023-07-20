95d4cb82884ee0e043b1dd467a3abb352afa42e8
Timur <agaev24@gmail.com>
2023-07-20 20:14:29 +0300

    Решение

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -3,3 +3,47 @@
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
 
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] result = new int[size];
+for (int i = 0; i < size; i++)
+{
+result[i] = new Random().Next(minValue, maxValue + 1);
+}
+return result;
+}
+
+int GetNum(string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+
+void NegativeArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = array[i] * -1;
+}
+Console.Write("Искомый массив: ");
+PrintArray(array);
+}
+
+int arrSize = GetNum("Задайте длину массива: ");
+int arrMin = GetNum("Введите минимальное значение массива: ");
+int arrMax = GetNum("Введите максимальное значение массива: ");
+
+int[] arr = GetArray(arrSize, arrMin, arrMax);
+
+PrintArray(arr);
+
+NegativeArray(arr);
